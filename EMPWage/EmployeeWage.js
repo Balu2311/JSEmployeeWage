58c8d343b34c0962cdfed404cb2a383cf00f5156
Vallem Balu <vbnreddy5@gmail.com>
2021-08-24 12:55:46 +0530

    Emp wage calculation for month

--- EMPWage/EmployeeWage.js
+++ EMPWage/EmployeeWage.js
@@ -3,6 +3,7 @@ const PART_TIME = 1;
 const WAGE_PER_HR  = 20;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
+const MAX_WORKING_DAYS_IN_MONTH = 20;
 let empHrs = 0;
 //genrate random value either 0 or 1
 let randomValue = Math.floor(Math.random() *3); 
@@ -11,16 +12,18 @@ function getWorkHours()
 {
     if (ABSENT==randomValue){
         console.log("Employee is Absent");
+        return 0
     }else if(PART_TIME==randomValue){
-    empHrs= 4
-    console.log(empHrs+' hr')
-    console.log("Employee is PartTime");
+        console.log("Employee is PartTime");
+        return 4
     } else{
-    empHrs = 8
-    console.log(empHrs+' hr')
-    console.log("Employee is FullTime")
+        console.log("Employee is FullTime")
+        return 8
     }
 }
-let empHrs1 =getWorkHours();
+for(let i=0;i<MAX_WORKING_DAYS_IN_MONTH;i++)
+{
+    empHrs += getWorkHours();
+}
 let totalWage = empHrs*WAGE_PER_HR;
 console.log('\nTotal Wage = '+totalWage);
