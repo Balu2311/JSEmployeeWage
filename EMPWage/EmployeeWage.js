d07745498bc472341fe4d3bff7239093cb53c538
Vallem Balu <vbnreddy5@gmail.com>
2021-08-24 13:10:17 +0530

    Emp wage calculation and days

--- EMPWage/EmployeeWage.js
+++ EMPWage/EmployeeWage.js
@@ -4,6 +4,8 @@ const WAGE_PER_HR  = 20;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const MAX_WORKING_DAYS_IN_MONTH = 20;
+const MAX_WORKING_HRS_IN_MONTH = 160;
+
 let empHrs = 0;
 //genrate random value either 0 or 1
 let randomValue = Math.floor(Math.random() *3); 
@@ -27,3 +29,14 @@ for(let i=0;i<MAX_WORKING_DAYS_IN_MONTH;i++)
 }
 let totalWage = empHrs*WAGE_PER_HR;
 console.log('\nTotal Wage = '+totalWage);
+
+let day=0;
+empHrs =0;
+while(day++ <MAX_WORKING_DAYS_IN_MONTH && empHrs<=MAX_WORKING_HRS_IN_MONTH)
+{
+    empHrs += getWorkHours();
+}
+console.log('\ntotal days = '+(day-1));
+console.log('total emp hours = '+empHrs);
+let totalWage1 = empHrs*WAGE_PER_HR;
+console.log('Total Wage1 = '+totalWage1);
