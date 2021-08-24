04305cdec5e3431af24b437c5bd7fa2fa40884a9
Vallem Balu <vbnreddy5@gmail.com>
2021-08-24 11:59:26 +0530

    Emp Present PartTime Absent

--- EMPWage/EmployeeWage.js
+++ EMPWage/EmployeeWage.js
@@ -1,6 +1,12 @@
 const ABSENT = 0;
-
+const PART_TIME = 1;
 //genrate random value either 0 or 1
-let randomValue = Math.floor(Math.random() *2); 
+let randomValue = Math.floor(Math.random() *3); 
 console.log('Random value = '+randomValue);
-console.log((ABSENT==randomValue)?"Employee is Absent":"Employee is present");
+if ((ABSENT==randomValue)){
+console.log("Employee is Absent");
+} else if(PART_TIME==randomValue){
+console.log("Employee is PartTime");
+} else{
+console.log("Employee is FullTime")
+}
