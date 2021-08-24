70cc63ec8198418a23c8895e33de439d44131ddd
Vallem Balu <vbnreddy5@gmail.com>
2021-08-24 18:28:57 +0530

    Valid Name taken

--- EMPWage/EmployeePayrollData.js
+++ EMPWage/EmployeePayrollData.js
@@ -18,8 +18,20 @@ class EmployeePayroll
     //getter and setter methods for properties of class
     get id() {return this.id};
     set id(id){this.id=id};
-    get name() { return this.name};
-    set name(name) { this.name = name};
+    get name() { return this._name};
+    set name(name)
+    {
+        //validating name while setting using regex
+        let regex = new RegExp('^[A-Z]{1}[a-z]{2,}$');
+        
+        if(regex.test(name))
+            this._name = name;
+            
+        else
+            throw "Error! Name format is incorrect";
+
+         
+    };
 
     get salary() {return this.salary};
     set salary(salary) { this.salary = salary};
@@ -35,10 +47,18 @@ class EmployeePayroll
 let empPayroll = new EmployeePayroll(1,"Reddy",10000);
 console.log(empPayroll.toString());
 //setting values to the properties of class
-empPayroll.id=2;
-empPayroll.name = "Balu";
-empPayroll.salary = 10000;
-console.log(empPayroll.toString());
+//setting values to the properties of class using try catch
+try
+{
+    empPayroll.id=2;
+    empPayroll.name = "Balu";
+    empPayroll.salary = 10000;
+    console.log(empPayroll.toString());
+}
+catch(e)
+{
+    console.error(e);
+}
 //extend gender and start date
 let emp = new EmployeePayroll(3,"Bhanu",11000,'M',new Date());
 console.log(emp.toString());
